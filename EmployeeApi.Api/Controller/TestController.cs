0e63a38188bb9116ab4af62fa56f68974f6713dd
Ravi Gupta <ravi.jeetendra@gmail.com>
2026-06-24 12:32:01 +0530

    Added TestController v1

---
+++ EmployeeApi.Api/Controller/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            Message = "Version 1",
+            Time = DateTime.Now
+        });
+    }
+}
